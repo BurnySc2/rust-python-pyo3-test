ebb5ebd748ded8828b5601c4550f55de7f5d61a2
BuRny <gamingburny@gmail.com>
2019-12-08 16:11:50 +0100

    Fix move command

--- move.py
+++ move.py
@@ -1,6 +1,6 @@
 import shutil
 import sys
 
-version = sys.argv[1].replace(".", " ")
+version = sys.argv[1].replace(".", "")
 
-shutil.copy("./target/release/my_library.dll", "./artifacts/my_lib.cp{}-win_amd64.pyd".format(version))
+shutil.move("./target/release/my_library.dll", "./artifacts/my_lib.cp{}-win_amd64.pyd".format(version))
